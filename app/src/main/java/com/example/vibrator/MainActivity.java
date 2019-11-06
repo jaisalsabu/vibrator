63c97a9350aa30f50ddb1e02bcbddd55af2dea0f
jaisalsabu <jaisal1010sabu@gmail.com>
2019-11-06 11:32:27 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vibrator/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.vibrator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    Vibrator vibrator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button2);
+        vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Build.VERSION.SDK_INT >= 26) {
+                    vibrator.vibrate(VibrationEffect.createOneShot(200, VibrationEffect.DEFAULT_AMPLITUDE));
+                } else {
+                    vibrator.vibrate(200);
+                }
+            }
+        });
+
+    }
+}
